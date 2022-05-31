f1f9b286f3d4d3ba9ee1afae118343edb0c0afaf
lipik <dlipeev@gmail.com>
2022-05-31 20:49:10 +0300

    остановка по Ctrl+C

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import sys
 import os
+import time
 
 
 bad_ips = []
@@ -65,40 +66,35 @@ if os.path.exists(ALREADY_BLOCKED_IPS_LIST):
 
 print(already_blocked_ips)
 
-
-
-i = 0
-
-for line in sys.stdin:
-    # print(line)
-    #line = f.readline()
-    i += 1
-    # if i == 10000:
-    #     break
-    # print(line)
-    # print(regex.match(line).groups())
-    try:
-        ip, date, request, response_code, response_size, referer, useragent = regex_main.match(line).groups()
-    except AttributeError:
-        break
-    # print(ip, date, request, response_code, response_size, referer, useragent)
-    parsed_request = request.split(' ')
-    try:
-        if parsed_request[1] == '/':
-            count_ip(ip)
-        if parsed_request[0] not in ALLOWED_METHODS:
+try:
+    for line in sys.stdin:
+        # print(line)
+        #line = f.readline()
+        try:
+            ip, date, request, response_code, response_size, referer, useragent = regex_main.match(line).groups()
+        except AttributeError:
+            break
+        # print(ip, date, request, response_code, response_size, referer, useragent)
+        parsed_request = request.split(' ')
+        try:
+            if parsed_request[1] == '/':
+                count_ip(ip)
+            if parsed_request[0] not in ALLOWED_METHODS:
+                add_ip(ip)
+        except IndexError:
+            #   эта ситуация обозначает что в параметрах запроса полное говнище со спецсимволами, таких сразу в печку
             add_ip(ip)
-    except IndexError:
-        #   эта ситуация обозначает что в параметрах запроса полное говнище со спецсимволами, таких сразу в печку
-        add_ip(ip)
-        continue
+            continue
+except KeyboardInterrupt:
+    pass
+finally:
+    print('останавливаемся...')
+    ip_freq_sorted = sorted(ip_freq.items(), key=lambda x: x[1], reverse = True)
+    for rec in ip_freq_sorted:
+        if rec[1] > DDOS_THRESHOLD:
+            add_ip(rec[0])
+    for ip in bad_ips:
+        print(ip)
+    blame_ips(bad_ips)
 
 
-ip_freq_sorted = sorted(ip_freq.items(), key=lambda x: x[1], git cdreverse = True)
-#print(ip_freq_sorted[0:10])
-for rec in ip_freq_sorted:
-    if rec[1] > DDOS_THRESHOLD:
-        add_ip(rec[0])
-for ip in bad_ips:
-    print(ip)
-blame_ips(bad_ips)
